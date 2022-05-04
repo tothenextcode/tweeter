62033178cf2a21cdda3e30fddb49f6fad137de49
JC Flores <tothenextcode@gmail.com>
2022-05-04 03:56:16 +0000

    Refactor counter and move function outside document.ready() for js best practice

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,13 @@
 $(() => {
-
-  $('textarea').on('input', function() {
-    const charLength = MAX_CHARS - $(this).val().length;
-    const $counter = $(this).siblings().children('.counter');
-
-    if (charLength < 0) {
-      $counter.addClass("red");
-    } else {
-      $counter.removeClass("red");
-    }
-
-    $counter.val(charLength);
-  });
-
+  $('textarea').on('input', updateCharCounter);
 });
+
+const updateCharCounter = function() {
+  const charLength = MAX_CHARS - $(this).val().length;
+  const $counter = $(this).siblings().children('.counter');
+  $counter.val(charLength);
+
+  if (charLength < 0) return $counter.addClass("red");
+    
+  $counter.removeClass("red");
+};
